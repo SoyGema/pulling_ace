c1646a0996208bc27294ec7bb9190d3ab434eead
Gema <gema.parreno.piqueras@gmail.com>
2023-11-07 22:31:12 +0100

    integrations

--- pulling_ace/cli.py
+++ pulling_ace/cli.py
@@ -9,42 +9,55 @@ from .utils.subprocessor import promptinjection, run_injections, riskcards, toxi
 def main():
     parser = argparse.ArgumentParser(description="PullingAce.")
     subparsers = parser.add_subparsers(dest="command")
+    # Subparser for tomato attack
+    tomato_parser = subparsers.add_parser("tomato")
+    tomato_parser.add_argument("--model", type=str, required=True, help="Model to use")
+    tomato_parser.add_argument("--dataset", type=str, required=True, help="Dataset to use")
+    tomato_parser.add_argument("--num-examples", type=int, default=10, help="Number of examples for the attack")
 
-    parser.add_argument("--attack", type=str, help="Attack type")
-    parser.add_argument("--model", type=str, help="Model to use")
-    parser.add_argument("--dataset", type=str, help="Dataset to use")
-    parser.add_argument(
-        "--num-examples", type=int, default=10, help="Number of examples for the attack"
-    )  # New argument
+    # Subparser for prompt injection attack
+    prompt_injection_parser = subparsers.add_parser("promptinjection")
+    prompt_injection_parser.add_argument("--model_type", type=str, required=True, help="Model type for subprocessor")
+    prompt_injection_parser.add_argument("--model", type=str, required=True, help="Model name for subprocessor")
+    prompt_injection_parser.add_argument("--probes", type=str, required=True, help="Probes for subprocessor")
 
-    # New parser for subprocessor
-    subprocess_parser = subparsers.add_parser("prompt_injection")
-    subprocess_parser.add_argument(
-        "--model_type", type=str, required=True, help="Model type for subprocessor"
-    )
-    subprocess_parser.add_argument(
-        "--model_name", type=str, required=True, help="Model name for subprocessor"
-    )
-    subprocess_parser.add_argument(
-        "--probes", type=str, required=True, help="Probes for subprocessor"
-    )
+    # ... (add other subparsers for riskcards, toxicity, etc.)
+    toxicity_parser = subparsers.add_parser("toxicity")
+    toxicity_parser.add_argument("--model_type", type=str, required=True, help="Model type for subprocessor")
+    toxicity_parser.add_argument("--model", type=str, required=True, help="Model name for subprocessor")
+    toxicity_parser.add_argument("--probes", type=str, required=True, help="Probes for subprocessor")
+
+    # ... (add other subparsers for riskcards, toxicity, etc.)
+    riskcards_parser = subparsers.add_parser("riskcards")
+    riskcards_parser.add_argument("--model_type", type=str, required=True, help="Model type for subprocessor")
+    riskcards_parser.add_argument("--model", type=str, required=True, help="Model name for subprocessor")
+    riskcards_parser.add_argument("--probes", type=str, required=True, help="Probes for subprocessor")
 
     args = parser.parse_args()
 
-    run_injections(args.model_type, args.model_name, args.probe_family)
+    #if args.command == "tomato":
+        #results = perform_tomato_attack(args.model, args.dataset, args.num_examples)
+        #print(results)
+    if args.command == "promptinjection":
+        promptinjection(args.model_type, args.model, args.probes)
+        run_injections(args.model_type, args.model, "promptinject")
 
-    if args.attack == "tomato":
-        results = perform_tomato_attack(args.model, args.dataset, args.num_examples)
-        print(results)
-    elif args.command == "prompt_injection":
-        promptinjection(args.model_type, args.model_name, args.probes)
-    elif args.command == "riskcards":
-        riskcards(args.model_type, args.model_name, args.probes)
     elif args.command == "toxicity":
-        toxicity(args.model_type, args.model_name, args.probes)
-    else:
-        print("Invalid command")
+        toxicity(args.model_type, args.model, args.probes)
+        run_injections(args.model_type, args.model, "realtoxicityprompts")
 
+    elif args.command == "riskcards":
+        riskcards(args.model_type, args.model, args.probes)
+        run_injections(args.model_type, args.model, "lmrc")
+
+    # ... (handle other subcommands)
+    else:
+        print("Invalid command or no command specified")
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
